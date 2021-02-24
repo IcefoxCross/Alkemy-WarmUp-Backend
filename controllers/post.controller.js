f71a2d541c09b0800400dfbfd5432239af80c774
Cesar <reuscesar9@gmail.com>
2021-02-24 12:19:32 -0300

    API - GET /posts/:id implementado

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -13,3 +13,19 @@ exports.getPosts = (req, res) => {
         res.status(200).send(posts);
     }).catch(err => res.status(500).send({message: err.message}));
 };
+
+exports.getPost = (req, res) => {
+    const _id = req.params.id;
+    Post.findOne({
+        where: {id: _id},
+        include: [{model: Category, attributes: ['name'], as: 'category'}],
+        attributes: {exclude: ['categoryId']},
+        raw: true
+    }).then(post => {
+        if (post) {
+            res.status(200).send(post);
+        } else {
+            res.status(404).send({message: 'Post not found.'});
+        }
+    }).catch(err => res.status(500).send({message: err.message}));
+};
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,4 +2,5 @@ const controller = require('../controllers/post.controller');
 
 module.exports = function(app) {
     app.get('/posts', controller.getPosts);
+    app.get('/posts/:id', controller.getPost);
 };
